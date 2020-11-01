a72d537537f98fad53e2f75545efc62cdd08f57b
Ernest-Epistola <ernestepistola@gmail.com>
2020-11-01 13:13:47 +0800

    Added child and builder properties to constructor

--- lib/src/framework/bluesy_service_provider.dart
+++ lib/src/framework/bluesy_service_provider.dart
@@ -1,10 +1,19 @@
 import 'package:bluesy/bluesy.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:provider/provider.dart';
 
 class BluesyServiceProvider extends ChangeNotifierProvider<BluesyService> {
-  BluesyServiceProvider({@required BluesyService service})
-      : super(
+  final Widget child;
+  final Widget Function(BuildContext, Widget) builder;
+
+  BluesyServiceProvider({
+    @required BluesyService service,
+    this.child,
+    this.builder,
+  }) : super(
           create: (_) => service,
+          child: child,
+          builder: builder,
         );
 }
