fc5df5dd8d61e969af1c1704b52d1a55620b0fc4
Ernest-Epistola <ernestepistola@gmail.com>
2020-11-02 17:33:11 +0800

    Added export statements for widgets

--- lib/bluesy.dart
+++ lib/bluesy.dart
@@ -1,4 +1,9 @@
 library bluesy;
 
+// Service
 export './src/framework/bluesy_service.dart';
 export './src/framework/bluesy_service_provider.dart';
+
+// Widgets
+export './src/widgets/bluesy_text.dart';
+export './src/widgets/bluesy_toggle_switch.dart';
